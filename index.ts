15ec21077197307a6d2275aa0431f9d714c93fb3
Jin Lin <jlin2001@hotmail.com>
2023-10-21 17:08:42 -0400

    update cors

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import http from 'http';
 import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, {
+  cors: { origin: 'https://jinlin-portfolio.web.app' },
+});
 const chats: { [key: string]: string } = {};
 
 io.on('connection', (socket) => {
