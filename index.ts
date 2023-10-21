62e7a3e9dd4cbd49c36490cd245b399c86ef412c
Jin Lin <jlin2001@hotmail.com>
2023-10-21 14:10:11 -0400

    update port

--- index.ts
+++ index.ts
@@ -47,6 +47,6 @@ io.of('/').adapter.on('delete-room', (roomId) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log('listening on port 3000');
+server.listen(80, () => {
+  console.log('listening on port 80');
 });
