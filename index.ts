bc227b9b642618afb6d24a9b24c60df7d4157f34
Jin Lin <jlin2001@hotmail.com>
2023-10-21 13:50:49 -0400

    update cors

--- index.ts
+++ index.ts
@@ -3,9 +3,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: process.env.CLIENT_ORIGIN },
-});
+const io = new Server(server);
 const chats: { [key: string]: string } = {};
 
 io.on('connection', (socket) => {
